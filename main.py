77769855b02303a4cf3433c30babd72337cecc29
Muelvzz <muelvinlopez25@gmail.com>
2025-06-06 17:52:56 +0800

    Added subject, and filter NEWS into english

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import send_email
 # Our personal API
 api_key = "0fc34c3d866047af97b8ce3de77d7391"
 
+# This is for the NEWS topic
+topic = "business"
+
 # The url that we are going to get data
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0fc34c3d866047af97b8ce3de77d7391"
+url = "https://newsapi.org/v2/top-headlines?country=us&" \
+    f"category={topic}" \
+    "&apiKey=0fc34c3d866047af97b8ce3de77d7391&language=en"
 
 request = requests.get(url)
 
@@ -15,8 +20,11 @@ content = request.json()
 
 # We've set-up a body variable to contain the strings. I set-up as an empty for now because we are going to fill that thing later in the for-loop
 body = ""
-for article in content["articles"]:
-    body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+for article in content["articles"][:20]:
+
+    if article["description"] is not None:
+        body = "Subject: Today's news" + "\n" + body + article["title"] + "\n" + \
+            article["description"] + "\n" + article['url'] + 2*"\n"
 
 # This is added to ensure that there are other values in the key that have unicode characters which python could not handle
 body = body.encode('utf-8')
