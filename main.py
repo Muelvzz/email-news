5012d5a12cc9f55fbbc6a4135f976b0a39253558
Muelvzz <muelvinlopez25@gmail.com>
2025-06-06 13:23:19 +0800

    Send email into my gmail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import send_email
 
 # Our personal API
 api_key = "0fc34c3d866047af97b8ce3de77d7391"
@@ -12,9 +13,13 @@ request = requests.get(url)
 # .json() method is used to convert that variable into a dictionary rather than a string
 content = request.json()
 
+# We've set-up a body variable to contain the strings. I set-up as an empty for now because we are going to fill that thing later in the for-loop
+body = ""
 for article in content["articles"]:
-    print(article["author"])
-    print(article["title"])
-    print(article["description"])
-    print(article["content"])
-    print(" ")
+    body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+# This is added to ensure that there are other values in the key that have unicode characters which python could not handle
+body = body.encode('utf-8')
+
+# We've sent the variable into the actual sending of the email in different file
+send_email.send_email(body)
